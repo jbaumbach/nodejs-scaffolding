428dc800fe99fdfabefbb2dc354f64438a423df2
jbaumbach <john.j.baumbach@gmail.com>
2013-02-03 23:45:46 -0800

    Minor updates to comments, test files

--- test/testRunner.js
+++ test/testRunner.js
@@ -2,10 +2,10 @@
     The vast majority of the time you will run your tests from the terminal with 
     the command:
 
-      $ mocha --recursive
+      $ mocha --recursive --reporter spec
     
     That runs all the tests that it finds in the "./test" directory, including all
-    subdirectories.
+    subdirectories, using the nice formatting provided by the "spec" reporter.
     
     Occasionally, a test will fail and you don't know why.  In that case, it's useful
     to debug the test and your code.  That's where this file comes in.
@@ -20,7 +20,11 @@
 //
 // Add the test file(s) you want to debug here.
 //
-var testFiles = ["./test/routes/testUser.js", "./test/common/testAuthorization.js"];
+//var testFiles = ["./test/routes/testUser.js"];
+var testFiles = ["./test/common/testAuthorization.js"];
+
+//var testFiles = ["./test/common/testGlobalFunctions.js"];
+//var testFiles = ["./test/data/testUserManager.js"];
 
 var Mocha = require('mocha');
 var mocha = new Mocha;
