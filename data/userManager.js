57e4b8948db1f614620c2ca8115a6a413a541443
jbaumbach <john.j.baumbach@gmail.com>
2013-02-03 23:51:37 -0800

    Added support for API user database calls

--- data/userManager.js
+++ data/userManager.js
@@ -16,7 +16,10 @@
 // todo: use a factory pattern to allow dependency injection.  We just return 
 // mongo by default now.
 //  
-var db = require('./drivers/mongoUserManager');
+var db = require('./drivers/mongoUserManager')
+  , globalFunctions = require('../common/globalFunctions')
+  , thisModule = this
+;
 
 exports.getUser = function(id, resultCallback) {
   db.getUser(id, resultCallback);
@@ -32,4 +35,49 @@ exports.validateCredentials = function(email, password, resultCallback) {
 
 exports.deleteUser = function(id, resultCallback) {
   db.deleteUser(id, resultCallback);
+}
+
+exports.getApiUser = function(apiKey, resultCallback) {
+  db.getApiUser(apiKey, resultCallback);
+}
+
+exports.getApiUserByUserId = function(userId, resultCallback) {
+  db.getApiUserByUserId(userId, resultCallback);
+}
+
+//
+// Insert or update an api user.  Note that if inserting an api user (e.g. the apiKey
+// field is empty), the apiKey and password fields will be generated and filled in
+// to the returned ApiUser object.
+//
+exports.upsertApiUser = function(apiUser, resultCallback) {
+  
+  var currentApiKey = apiUser.apiKey || '';
+  var isInserting = currentApiKey === '';
+
+  db.upsertApiUser(apiUser, function(newApiUser) {
+    
+    if (isInserting) {
+      //
+      // We have more work to do - let's generate some values based off the new id
+      // we got from the DB that's guaranteed to be unique.  MD5 is good enough to
+      // use to generate unique 32 character values, plus it would make it harder
+      // on an attacker to guess sequential UIDs
+      //
+      var uniqueVal = newApiUser.id;
+      var uniqueKeys = globalFunctions.generateUniqueCredentials(uniqueVal);
+
+      newApiUser.apiKey = uniqueKeys.uid;
+      newApiUser.password = uniqueKeys.password;
+      
+      thisModule.upsertApiUser(newApiUser, resultCallback);
+
+    } else {
+      resultCallback(newApiUser);
+    }    
+  })
+}
+
+exports.deleteApiUser = function(apiKey, resultCallback) {
+  db.deleteApiUser(apiKey, resultCallback);
 }
