afff12c9975cc32f053982f77e24878c0953fc98
jbaumbach <john.j.baumbach@gmail.com>
2013-02-03 23:23:36 -0800

    Added ApiUser model

---
+++ models/apiUser.js
@@ -0,0 +1,24 @@
+/**
+ * Defines properties for an api user.  
+ * 
+ * See architecture notes in "user.js"
+ */
+
+var ApiUser = function(values) {
+
+  values = values || {};
+  
+  this.id = values.id || '';
+  
+  //
+  // Optional - for convenience, apiUser is associated with regular user accounts.
+  //
+  this.associatedUserId = values.associatedUserId || '';
+  
+  this.apiKey = values.apiKey || '';
+  this.password = values.password || '';
+  this.authorizedOperations = values.authorizedOperations || [];
+  this.inactiveDate = values.inactiveDate;
+}
+
+module.exports = ApiUser;
